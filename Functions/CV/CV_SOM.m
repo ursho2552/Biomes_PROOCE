79a7ff1627ac08a9cc4212140773db4b998697f2
ursho2552 <urs.hofmann@usys.ethz.ch>
2023-10-17 12:24:46 +0200

    Adapted Euler_run_CV.sh to slurm, and changed the function CV_SOM to reduce hardcoded paths

--- Functions/CV/CV_SOM.m
+++ Functions/CV/CV_SOM.m
@@ -1,12 +1,14 @@
-function CV_SOM(fr,Num)
+function CV_SOM(fr, Num, directory_data, file_partition, directory_output)
 %This traines a SOM for the cross-validation experiment based
 
 %{
 Parameters:
     fr (int): Fraction of data leave-out fraction. Note that the value
-    is only a name and not the fraction itself. This is not ideal, but will
-    leave it as is for the moment
+    is only a name and not the fraction itself. 
     Num (int): ID of fold combination used
+    directory_data (char): Path to observations
+    file_partition (char): Path to partition of the data
+    directory_output (char): Path for output
  
  Output:
     None. This function saves the output directly to a specified directory.
@@ -15,17 +17,13 @@ Parameters:
 %}
 
 %load data
-cd('/net/kryo/work/ursho/PhD/Projects/Biomes/Scripts/Biomes_PROOCE/Data/00Probabilities')
-load('Simple_sort_Data.mat')
+load(directory_data)
 
 optimal_dim = [31 31];
 optimal_epoch = 200;
 
-
 %load partitions
-cd('/net/kryo/work/ursho/PhD/Projects/Biomes/Scripts/Biomes_PROOCE/Data/03CrossValidation/Folds/')
-str = horzcat('Data_partitioning_cross_validation_fr_',int2str(fr),'_Seed_7.mat')
-load(str)
+load(file_partition)
 
 %select which  partition out of the 10
 training_flag = idxtrain(:,Num);
@@ -34,14 +32,16 @@ training_flag = idxtrain(:,Num);
 noisy_data = No_nan_phyto_simple(training_flag == 1,:);
 
 
-
 [classes_noise, noise_net] = My_SOM( noisy_data, optimal_dim(1),...
     optimal_dim(2),optimal_epoch,'mandist' );
 
-str1 = horzcat('CV_Fr_',int2str(fr),'_Num_',int2str(Num));
 
-cd('/net/kryo/work/ursho/PhD/Projects/Biomes/Scripts/Biomes_PROOCE/Data/03CrossValidation/SOMs')
+
+cd(char(directory_output))
+
+str1 = horzcat('CV_Fr_',int2str(fr),'_Num_',int2str(Num));
 save(str1,'classes_noise','noise_net','fr','Num','training_flag')
+
 exit
 
 end
