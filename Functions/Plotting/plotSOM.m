4482f2bc73dbb2d5e8b94ea64e2b4320a00e97d3
Urs Hofmann Elizondo <43608472+ursho2552@users.noreply.github.com>
2023-11-29 08:43:59 +0100

    Update plotSOM.m
    Removed comments and unused variables from plotSOM function

--- Functions/Plotting/plotSOM.m
+++ Functions/Plotting/plotSOM.m
@@ -1,4 +1,4 @@
-function [h,Fig,cmap2] = plotSOM( new_data,month,nr_classes)%,p)
+function [h,Fig,cmap2] = plotSOM( new_data,month,nr_classes)
 %Function to plot a map
 
 %{
@@ -17,33 +17,19 @@ Parameters:
 
 %}
 
-
-
-
 lons = -179.5:179.5;
 lats = -89.5:89.5;
-%latlim = [-89.5 89.5]; % set map limits for plots
-latlim = [-78 80];
-%lonlim = [-179.5 179.5];
-lonlim = [20.5 379.5];
-h = figure('color','white');
-%subplot(1,2,1,'Parent',p)
-hold on
 
-% axesm('eqdcylin','MapLatLimit',latlim,'MapLonLimit',lonlim, ...
-%    'Frame','on',...
-%    'FEdgeColor',[0.5 0.5 0.5],'FLineWidth',0.5,...
-%    'MeridianLabel','on','ParallelLabel','on', 'FEdgeColor',[0.5 0.5 0.5],'FLineWidth',0.5,'FontSize',12);
-%'eckert4'
+h = figure('color','white');
+hold on
 
 axesm('robinson','MapLatLimit',latlim,'MapLonLimit',lonlim, ...
    'Frame','on','Grid','on','MeridianLabel','on','ParallelLabel','on')%...
-%    'FEdgeColor',[0.5 0.5 0.5],'FLineWidth',0.5,...
-%    'MeridianLabel','on','ParallelLabel','on', 'FEdgeColor',[0.5 0.5 0.5],'FLineWidth',0.5,'FontSize',12);
+   
 axis off
 setm(gca,'MLabelLocation',60,'LabelFormat','none','MLabelParallel','South');
 setm(gca,'PLabelLocation',45,'LabelFormat','none');
-%geoshow('landareas.shp','EdgeColor',[0.5 0.5 0.5], 'FaceColor', [0.5 0.5 0.5]); % grey land mass
+
 load coastlines
 plotm(coastlat,coastlon,'k')
 
@@ -53,64 +39,39 @@ nlat = length(lats);
 nlon = length(lons);
 
 Fig = NaN(nlat,nlon); %add one for the definition of the point 0.5 or 180.5
+
 if(month < 13)
     for i=1:nlat
         for j=1:nlon
             Fig(i,j) = new_data(month,i,j);
         end
     end
-    %Fig(:,361) = new_data(month,:,1);
+    
 else
     for i=1:nlat
         for j=1:nlon
             Fig(i,j) = new_data(1,i,j);
         end
     end
-    %Fig(:,361) = new_data(1,:,1);
 end
-sum(isnan(Fig(:,1)));
-sum(isnan(Fig(:,end)));
 
 [lat,lon] = meshgrat(lats,round(lons));
 
 t = geoshow(double(lat),double(lon),Fig,'DisplayType','texturemap') 
 set(t,'FaceAlpha','texturemap','AlphaData',double(~isnan(Fig)));
-% geoshow(double(lat),double(lon),Fig,'DisplayType','contour','LevelList',0.5,'LineColor','red')
-% geoshow(double(lat),double(lon),Fig,'DisplayType','contour','LevelList',1,'LineColor','magenta')
-% land = shaperead('landareas', 'UseGeoCoords', true)
 
-% geoshow(land, 'FaceColor', [1 1 1])
-
-aa = unique(Fig(~isnan(Fig)));
-size(Fig);
-% [lat,lon] = meshgrat(latchl,[lonchl;180.5]);
-% surfm(lat,lon,Fig);
-%surfm(double(latchl),double([lonchl]), Fig);
-%surf(double(latchl),double([lonchl]),Fig);
-%jj = input('dfd')
-% contour(lon,lat, Fig,[0.5,1,1.5,2])
-%colorbar
-%
 if(isnan(nr_classes))
     colormap(parula);
-    %colormap(parula(nr_classes))
-    %caxis([1 nr_classes])
+    
 elseif(strcmp(nr_classes,'black'))
     colormap(gray(1));
+    
 else
     cmap = parula(nr_classes);
     [cmap_tmp] = shuffle_colormap(cmap);
     [cmap_tmp] = shuffle_colormap(cmap_tmp);
     %colormap(cmap)
     cmap2 = colormap(cmap_tmp);
-%colorbar%('Ticks', [aa])
+
 caxis([1 nr_classes]);
 end
-%caxis([aa(1) aa(end)])
-%cb = colorbar;
-%aa = unique(Fig(~isnan(Fig)));
-%set(cb, 'ytick', 1:aa(end), 'yticklabel', cellstr(num2str(aa))); 
-%colorbar('Ticks',[-5,-2,1,4,7]
-%}
-
-
